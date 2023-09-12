2dc24a4da1df6f2a3e895078781f70be76c2dada
David Backeus <duztdruid@gmail.com>
2023-09-12 14:27:14 +0200

    Mount the Sidekiq UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
+require "sidekiq/web"
+
 Rails.application.routes.draw do
+  mount Sidekiq::Web => "/sidekiq"
+
   resources :links
   resources :posts
 
@@ -7,6 +11,7 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
 
   # Defines the root path route ("/")
   root "posts#index"
