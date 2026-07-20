272d415e595114fe26b74cd3a9ec0e87ec1b1f12
David Backeus <duztdruid@gmail.com>
2026-07-20 11:56:06 +0200

    Skip Sidekiq (+redis) in test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -25,6 +25,9 @@ Rails.application.configure do
   # Render exception templates for rescuable exceptions and raise for other exceptions.
   config.action_dispatch.show_exceptions = :rescuable
 
+  # Enqueue jobs in memory instead of Sidekiq so tests don't require Redis.
+  config.active_job.queue_adapter = :test
+
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
 
